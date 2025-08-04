7331c31e0d48eb8d21888f2bc8d5a0ab4de46c25
Khyati Choubey <manasichoubey4@gmail.com>
2025-08-04 22:30:39 +0530

    04-08-25

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const generatecompchoice = () => {
 }
 
 const DrawGame = () =>{
-    console.log("It's a draw!");
     msg.innerText = "It was a draw!";
     msg.style.backgroundColor = "darkslateblue";
 }
@@ -30,22 +29,18 @@ const ShowWinnner = (userWin,userchoice, compChoice) => {
     if(userWin){
         userscore++;
         userScorePara.innerText = userscore;
-        console.log("UserWins");
         msg.innerText = "You Win!";
         msg.style.backgroundColor = "darkcyan";
     }else{
         computerscore++;
         computerScorePara.innerText = computerscore;
-        console.log("Comp wins");
         msg.innerText = "Computer Wins!";
         msg.style.backgroundColor = "indianred";
     }
 }
 
 const playgame = (userchoice)=>{
-    console.log("User choice = ",userchoice);
     const compChoice = generatecompchoice();
-    console.log("Computer choice = ", compChoice);
 
     if (userchoice == compChoice){
         DrawGame();
